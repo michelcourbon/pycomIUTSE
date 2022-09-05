e81417fd610e86632d5ede05f645b99de9d59fcc
Michel Courbon <mcour42@gmail.com>
2022-09-05 11:21:45 +0200

    2022-09 idem mqtt config for mqttPysense applciation

--- 6-mqttPysense/config.py
+++ 6-mqttPysense/config.py
@@ -3,13 +3,9 @@
 # Copyright (c) 2022, IUT / Université Jean-Monnet
 #
 
-# WIFI_SSID = 'ubuntuMP'
-# WIFI_PASS = 'qjyWU5yEWjTdyVng'
-WIFI_SSID = 'SFR-5488'
-WIFI_PASS = 'DDZ1I4PVH5TW'
+WIFI_SSID = 'ubuntuMP'
+WIFI_PASS = 'qjyWU5yEWjTdyVng'
 
-MQTT_SERVER = "192.168.0.39"
-# MQTT_USER = "iutmp"
-MQTT_USER = "user"
-# MQTT_PASS = "iutseMP2022"
-MQTT_PASS = "fgh123"
+MQTT_SERVER = "131.3.86.93"
+MQTT_USER = "iutmp"
+MQTT_PASS = "iutseMP2022"
